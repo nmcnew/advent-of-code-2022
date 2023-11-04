3b4650c7f108e20b34563607302a43b1c4e3a5ff
nmcnew <nmcnew@proton.me>
2023-11-04 12:53:15 -0600

    Orchestrate the following days so I don't have to think about it later

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::io::ErrorKind;
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
+    
     if args.init {
         fs::create_dir("data")
             .expect("Couldn't create data path");
@@ -26,10 +27,64 @@ fn main() -> std::io::Result<()> {
             i += 1;
         }
     }
-    match args.day {
-        1 => run_day_01(),
-        _ => println!("Did not find a day command"),
+    else if args.each {
+        run_day_01();
+        run_day_02();
+        run_day_03();
+        run_day_04();
+        run_day_05();
+        run_day_06();
+        run_day_07();
+        run_day_08();
+        run_day_09();
+        run_day_10();
+        run_day_11();
+        run_day_12();
+        run_day_13();
+        run_day_14();
+        run_day_15();
+        run_day_16();
+        run_day_17();
+        run_day_18();
+        run_day_19();
+        run_day_20();
+        run_day_21();
+        run_day_22();
+        run_day_23();
+        run_day_24();
+        run_day_25();
+        return Ok(()); //Exit early
     }
+    else {
+        match args.day {
+             1 => run_day_01(),
+             2 => run_day_02(),
+             3 => run_day_03(),
+             4 => run_day_04(),
+             5 => run_day_05(),
+             6 => run_day_06(),
+             7 => run_day_07(),
+             8 => run_day_08(),
+             9 => run_day_09(),
+            10 => run_day_10(),
+            11 => run_day_11(),
+            12 => run_day_12(),
+            13 => run_day_13(),
+            14 => run_day_14(),
+            15 => run_day_15(),
+            16 => run_day_16(),
+            17 => run_day_17(),
+            18 => run_day_18(),
+            19 => run_day_19(),
+            20 => run_day_20(),
+            21 => run_day_21(),
+            22 => run_day_22(),
+            23 => run_day_23(),
+            24 => run_day_24(),
+            25 => run_day_25(),
+            _ => println!("Did not find a matching day"),
+        }
+    } 
     Ok(())
 }
 
@@ -43,6 +98,9 @@ struct Args {
     /// Initialize the data files needed for advent of code
     #[arg(short, long, default_value_t=false)]
     init: bool,
+    /// Runs every solution
+    #[arg(short, long, default_value_t=false)]
+    each: bool,
 }
 
 fn run_day_01(){
@@ -53,4 +111,76 @@ fn run_day_01(){
     println!("You should ask elf {} for some food, they have {} calories on them", elf.0, elf.1);
     let elves = solutions::day_01::get_packrats(values);
     println!("The biggest three packs are {}, {}, and {}, for a total of {}", elves[0], elves[1], elves[2], elves.iter().sum::<i32>());
+}
+fn run_day_02(){
+    println!("Day 02 is not implented yet");
+}
+fn run_day_03(){
+    println!("Day 03 is not implented yet");
+}
+fn run_day_04(){
+    println!("Day 04 is not implented yet");
+}
+fn run_day_05(){
+    println!("Day 05 is not implented yet");
+}
+fn run_day_06(){
+    println!("Day 06 is not implented yet");
+}
+fn run_day_07(){
+    println!("Day 07 is not implented yet");
+}
+fn run_day_08(){
+    println!("Day 08 is not implented yet");
+}
+fn run_day_09(){
+    println!("Day 09 is not implented yet");
+}
+fn run_day_10(){
+    println!("Day 10 is not implented yet");
+}
+fn run_day_11(){
+    println!("Day 11 is not implented yet");
+}
+fn run_day_12(){
+    println!("Day 12 is not implented yet");
+}
+fn run_day_13(){
+    println!("Day 13 is not implented yet");
+}
+fn run_day_14(){
+    println!("Day 14 is not implented yet");
+}
+fn run_day_15(){
+    println!("Day 15 is not implented yet");
+}
+fn run_day_16(){
+    println!("Day 16 is not implented yet");
+}
+fn run_day_17(){
+    println!("Day 17 is not implented yet");
+}
+fn run_day_18(){
+    println!("Day 18 is not implented yet");
+}
+fn run_day_19(){
+    println!("Day 19 is not implented yet");
+}
+fn run_day_20(){
+    println!("Day 20 is not implented yet");
+}
+fn run_day_21(){
+    println!("Day 21 is not implented yet");
+}
+fn run_day_22(){
+    println!("Day 22 is not implented yet");
+}
+fn run_day_23(){
+    println!("Day 23 is not implented yet");
+}
+fn run_day_24(){
+    println!("Day 24 is not implented yet");
+}
+fn run_day_25(){
+    println!("Day 25 is not implented yet");
 }
